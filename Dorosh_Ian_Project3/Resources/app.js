4653d574365928466b1817e104fc4e5276da26ae
Ian Dorosh <iDorosh@fullsail.edu>
2014-07-21 19:28:26 -0500

    Project 3

--- Dorosh_Ian_Project3/Resources/app.js
+++ Dorosh_Ian_Project3/Resources/app.js
@@ -1,40 +1,129 @@
 Titanium.UI.setBackgroundColor('#000');
 
 
+var imagesFolder = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "photos");
+var imageFiles = imagesFolder.getDirectoryListing();
 
-var pWidth = Ti.UI.Platform.displayCaps.platformWidth;
-var pHeight = Ti.UI.Platform.displayCaps.platformHeight;
-var itemCount = 15;
-var rowCount = 3;
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformHeight;
+var rowCount = 4;
 var margin = 10;
-var trueCanvasWidth = ;
-var size = 
+var trueCanvasWidth = pWidth-margin;
+var size = trueCanvasWidth/rowCount-margin;
+
 
 var mainWindow = Ti.UI.createWindow({
-	title: "My First App",
+	title: "My Photos",
 	backgroundColor: "#fff",
 });
 
-var border = Ti.UI.createView({
-	backgroundColor: "#cecece",
-	width: pWidth,
-	top: 20,
+var openGalleryButton = Ti.UI.createView({
+	backgroundColor: "000",
+	opacity: .5,
+	height: 60,
+	width: 200,	
+	borderRadius: 5,
+	
 });
 
-var viewContainer = Ti.UI.createView({
-	width: pWidth,
-	backgroundColor :"#fef",
+var openGalleryText = Ti.UI.createLabel({
+	text: "Open Gallery",
+	color: "#fff",
 });
 
-for(var i=0; i<itemCount; i++){
+openGalleryButton.addEventListener("click", function(open){
+	navWindow.openWindow(galleryView);
+});
+
+openGalleryButton.add(openGalleryText);
+mainWindow.add(openGalleryButton);
+
+
+
+
+
+var galleryView = Ti.UI.createWindow({
+	title: "Gallery",
+	backgroundColor: "#fff",
+	layout : "horizontal",
+});
+
+
+var viewContainer = Ti.UI.createScrollView({
+	top:0,
+	width: pWidth,
+	backgroundColor :"#fff",
+	layout: "horizontal",
+	contentWidth: pWidth,
+	height: pHeight,
+	showVerticalScrollIndicator: true,
+});
+
+
+
+
+
+for(var i=0; i<imageFiles.length; i++){
 	var view = Ti.UI.createView({
-		backgroundColor: "#33ccff",
+		backgroundColor: "#fff",
 		top: margin,
+		left: margin,
 		width: size,
-		height: size
+		height: size,
+		borderRadius: 5,
 	});
-}
-mainWindow.add(viewContainer, border);
-mainWindow.open();
+	
+	var newImage = Ti.UI.createImageView({
+		title: imageFiles[i],
+		image: "photos/" + imageFiles[i],
+		top: 0,
+		width: view.width*2,
+	});
+	view.add(newImage);
+	viewContainer.add(view);
+};
+
+viewContainer.addEventListener("click", function(event){
+		photoView(event.source.title);
+});
+
+
+
+
+
+var photoView = function(data){
+	var detailWindow = Ti.UI.createWindow({
+		backgroundColor: "#fff",
+		title: data,
+	});
+	
+
+	var pictures = Ti.UI.createImageView({
+		image: "photos/" + data,
+		width: pWidth,
+		enableZoomControls: true,
+	});
+	
+
+	var closeWindow = function(){
+		detailWindow.close();
+	};
+	
+	navWindow.openWindow(detailWindow);
+	
+	detailWindow.add(pictures);
+	detailWindow.open();
+	
+};
+
+
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: mainWindow,
+});
+
+
+
+galleryView.add(viewContainer);
+navWindow.open();
 
  
